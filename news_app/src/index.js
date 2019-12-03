6c0d444ae8dc6a4efaadd383c4af0bda053b5d21
Peter Sarll <petersarll@gmail.com>
2019-12-02 18:55:29 -0800

    removed the createlink on every start of node, added console log that shows the current information in database

--- news_app/src/index.js
+++ news_app/src/index.js
@@ -7,16 +7,10 @@ const Link = require('./resolvers/Link')
 const Subscription = require('./resolvers/Subscription')
 const Vote = require('./resolvers/Vote')
 
-async function main() {
-  // Create a new link
-  const newLink = await prisma.createLink({ 
-    url: 'www.prisma.io',
-    description: 'Prisma replaces traditional ORMs',
-  })
-  console.log(`Created new link: ${newLink.url} (ID: ${newLink.id})`)
-
+async function main(){
   // Read all links from the database and print them to the console
   const allLinks = await prisma.links()
+  console.log('these are the following database entries')
   console.log(allLinks)
 }
 main().catch(e => console.error(e))
