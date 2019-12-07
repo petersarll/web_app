02f8cf28e08ff0de5bd785b30a1a4b76428a51ce
Peter Sarll <petersarll@gmail.com>
2019-12-07 15:22:01 -0800

    creating a link list

--- news_app/news-react-apollo/src/components/LinkList.js
+++ news_app/news-react-apollo/src/components/LinkList.js
@@ -26,7 +26,9 @@ class LinkList extends Component {
     
           return (
             <div>
-              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+              {linksToRender.map((link, index) => (
+                <Link key={link.id} link={link} index={index} />
+              ))}
             </div>
           )
         }}
